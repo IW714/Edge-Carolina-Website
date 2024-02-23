2547a287aa60646461a019646c11514157502a99
Weston Voglesonger <111527221+tvogle@users.noreply.github.com>
2024-02-23 23:10:26 +0000

    Added git config commands to new Dockerfile in .devcontainer, also began adding documentation for unboarding new developers.

--- app.py
+++ app.py
@@ -2,10 +2,10 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See LICENSE in the project root for license information.
 #-----------------------------------------------------------------------------------------
+from fastapi import FastAPI
 
-from flask import Flask
-app = Flask(__name__)
+app = FastAPI()
 
-@app.route("/")
-def hello():
-    return app.send_static_file("index.html")
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
